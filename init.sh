010dd3fad7e7ecb47b2eb5db57a4853f302a543c
hrssilva <haroldo.rssilva@gmail.com>
2023-12-02 16:12:25 -0300

    init script

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+kubectl aplly -f services/
+kubectl aplly -f monitors/
+
+INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+
+echo $GATEWAY_URL
+minikube tunnel
