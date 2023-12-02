16cac88d87d4892d8d199e3f431855d589c5eef1
hrssilva <haroldo.rssilva@gmail.com>
2023-12-02 17:34:19 -0300

    removed tunnel from init script

--- init.sh
+++ init.sh
@@ -2,10 +2,4 @@
 kubectl apply -f services/
 kubectl apply -f monitors/
 
-INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
-SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
-GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
-
-echo $GATEWAY_URL
-minikube tunnel
+kubectl -n istio-system get service istio-ingressgateway
