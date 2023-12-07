9ca51a823a78ead4b2b2f48ff86649942cae319b
hrssilva <haroldo.rssilva@gmail.com>
2023-12-06 23:31:10 -0300

    added circuit break test to fix_load

---
+++ fix_load/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+FORTIO_POD=$(kubectl get pods -l app=fortio -o 'jsonpath={.items[0].metadata.name}')
+kubectl exec "$FORTIO_POD" -c fortio -- /usr/bin/fortio curl -quiet http://httpbin:8000/get
