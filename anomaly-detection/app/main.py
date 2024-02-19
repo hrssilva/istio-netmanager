340efd41394109d16a575eb1553a333eada3f30b
hrssilva <haroldo.rssilva@gmail.com>
2024-02-19 16:06:15 -0300

    initial non clusterized version

--- anomaly-detection/app/main.py
+++ anomaly-detection/app/main.py
@@ -2,18 +2,18 @@ from os import environ
 from time import localtime, sleep
 import anomaly_detection as ad
 import data_query as dq
-from prometheus_client import start_http_server, Summary, gauge, counter
+from prometheus_client import start_http_server, Summary, Gauge, Counter
 
 
 if __name__ == "__main__":
     try:
-        prometheus = environ['PROMETHEUS_ADDRESS'] + ':' + environ['PROMETHEUS_PORT']
+        prometheus = environ['PROMETHEUS_ADDRESS'].strip("'") + ':' + environ['PROMETHEUS_PORT'].strip("'")
         prev_outliers = set()
         while(True):
             start_http_server(8080)
-            g = gauge('Current Anomalies','Current anomalies on network')
-            n = gauge('New Anomalies', 'New detected anomalies on network')
-            c = counter('Detected Anomalies','Anomalies detected since program started')  
+            g = Gauge('Current_Anomalies','Current anomalies on network')
+            n = Gauge('New_Anomalies', 'New detected anomalies on network')
+            c = Counter('Detected_Anomalies','Anomalies detected since program started')  
             data = dq.getPrometheusData(prometheus)
             outliers = ad.getOutliers(data) # Index of outliers in 'data'
             new_outliers = outliers - prev_outliers
