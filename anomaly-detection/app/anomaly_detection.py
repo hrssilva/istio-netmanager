fb23075d3261aa88ff3c38b6226e80446d3ca3eb
Rafael Trevisan <rafa.trevi@hotmail.com>
2024-02-19 20:06:19 -0300

    more bugfixes

--- anomaly-detection/app/anomaly_detection.py
+++ anomaly-detection/app/anomaly_detection.py
@@ -1,7 +1,7 @@
 from sklearn.neighbors import LocalOutlierFactor
 
 def getOutliers(data, neighbors):
-    if data.len > neighbors:
+    if len(data) > neighbors:
         lof = LocalOutlierFactor(n_neighbors=neighbors)
         return set([idx for idx, e in enumerate(lof.fit_predict(data)) if e < 0])
     return set()
--- anomaly-detection/app/main.py
+++ anomaly-detection/app/main.py
@@ -4,6 +4,7 @@ import anomaly_detection as ad
 import data_query as dq
 import sys
 from prometheus_client import start_http_server, Gauge, Counter
+import traceback
 
 
 if __name__ == "__main__":
@@ -14,18 +15,19 @@ if __name__ == "__main__":
         try:
             prev_outliers = set()
             data = []
-            start_http_server(8080)
+            start_http_server(9090)
             g = Gauge('Current_Anomalies','Current anomalies on network')
             n = Gauge('New_Anomalies', 'New detected anomalies on network')
-            c = Counter('Detected_Anomalies','Anomalies detected since program started')  
+            c = Counter('Detected_Anomalies','Anomalies detected since program started')
             while(True):
                 x, y = dq.getPrometheusData(prometheus)
-                print("Data Queried" + str(x) + " " + str(y))
+                print("Data Queried " + str(x) + " " + str(y))
                 data.append([x, y])
                 outliers = ad.getOutliers(data, neighbors) # Index of outliers in 'data'
+                print("Data Queried " + str(x) + " " + str(y))
                 new_outliers = outliers - prev_outliers
                 if len(outliers) > 0:
-                    print("Anomaly Ocurred",[data[o] for o in outliers])
+                    print("Anomaly Ocurred ",[data[o] for o in outliers])
                     c.inc()
                     g.set(len(outliers))
                     n.set(new_outliers)
@@ -33,5 +35,6 @@ if __name__ == "__main__":
                 sleep(wait_time)
         except :
             print('Unknown Error')
+            traceback.print_exc()
     except KeyError:
         print('Environment Error')
